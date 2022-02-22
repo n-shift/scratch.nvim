95a533ac1042b2995b1386af73a4d1ea857339c0
shift-d <shiftkeydeveloper@gmail.com>
2022-02-22 14:58:13 +0300

    feat(buffer): set scratch buffer name

--- lua/scratch/buffer.lua
+++ lua/scratch/buffer.lua
@@ -2,6 +2,7 @@ local buffer = {}
 
 function buffer.create(filetype)
     buffer.bufnr = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_buf_set_name(buffer.bufnr, "scratch.nvim")
     vim.api.nvim_buf_set_option(buffer.bufnr, "filetype", filetype)
     vim.api.nvim_win_set_buf(0, buffer.bufnr)
 end
