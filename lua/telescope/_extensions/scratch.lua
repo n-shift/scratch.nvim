4082974d4ebdf661c9bb49977a2a2d3832f0cda1
shift-d <shiftkeydeveloper@gmail.com>
2022-02-22 14:31:32 +0300

    feat(picker): set filetype

--- lua/telescope/_extensions/scratch.lua
+++ lua/telescope/_extensions/scratch.lua
@@ -29,8 +29,29 @@ local picker = function(opts)
     }):find()
 end
 
+local ft = function(opts)
+    opts = opts or {}
+    pickers.new(opts, {
+        prompt_title = "scratch.nvim filetype",
+        finder = finders.new_table({
+            results = filetypes
+        }),
+        sorter = config.generic_sorter(opts),
+        attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local selection = state.get_selected_entry()
+                local bufnr = require("scratch.buffer").bufnr
+                vim.api.nvim_buf_set_option(bufnr, "filetype", selection[1])
+            end)
+            return true
+        end
+    }):find()
+end
+
 return require("telescope").register_extension({
     exports = {
-        scratch = picker
+        scratch = picker,
+        ft = ft,
     }
 })
