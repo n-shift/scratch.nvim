fe15a015d45ae320a3fb468bf8d356b7f7b5cb69
shift-d <shiftkeydeveloper@gmail.com>
2022-02-22 14:25:09 +0300

    feat(picker): working picker

---
+++ lua/telescope/_extensions/scratch.lua
@@ -0,0 +1,36 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local config = require("telescope.config").values
+local actions = require("telescope.actions")
+local state = require("telescope.actions.state")
+
+local filetypes = {}
+for filetype, _ in pairs(require("scratch.eval")) do
+    table.insert(filetypes, filetype)
+end
+table.sort(filetypes)
+
+local picker = function(opts)
+    opts = opts or {}
+    pickers.new(opts, {
+        prompt_title = "scratch.nvim",
+        finder = finders.new_table({
+            results = filetypes
+        }),
+        sorter = config.generic_sorter(opts),
+        attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local selection = state.get_selected_entry()
+                require("scratch.buffer").create(selection[1])
+            end)
+            return true
+        end
+    }):find()
+end
+
+return require("telescope").register_extension({
+    exports = {
+        scratch = picker
+    }
+})
